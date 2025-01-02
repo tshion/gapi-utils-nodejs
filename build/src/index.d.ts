cee0103609c0ff96cf90b834625eef749fac3357
tshion <tenta_shion@yahoo.co.jp>
2025-01-02 21:04:53 +0900

    リリース戦略を見直すため、一旦リセット

--- build/src/index.d.ts
+++
@@ -1,1 +0,0 @@
-export { SheetsUtil } from './utils/sheets.util';
--- build/src/utils/core.util.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function authorizeByJwt(pathJwtFile: string, scopes?: string | string[]): Promise<import("google-auth-library").JWT>;
-export declare function isSameFloat(left: string, right: string): boolean;
